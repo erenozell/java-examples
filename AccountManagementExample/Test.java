eb0421bfd181946765702680b2919a5db42a0d62
erenozell <erenozel45@gmail.com>
2023-01-28 20:43:40 +0300

    B
    XD

--- AccountManagementExample/Test.java
+++ AccountManagementExample/Test.java
@@ -3,7 +3,12 @@ public class Test {
     public static void main(String[] args) {
 
         UserRepository repo = new UserRepository("gloriaJeans", 50);
+        repo.addUser(null);
+        repo.addUser(null);
 
-        
+        repo.addUser(null);
+
+        repo = new UserRepository("pause", 20);
+
     }
 }
--- AccountManagementExample/UserRepository.java
+++ AccountManagementExample/UserRepository.java
@@ -9,30 +9,53 @@
  * deleteUser-> userList içerisindeki kullanıcıyı verilen username (String) siler. 
  */
 
-
 public class UserRepository {
     private String name;
     private int index = 0;
     private User[] userList;
-    
-// {null,null,null,...}
-// null,null,null
-// Obj, null,null,null
 
-    UserRepository(String name, int length){
-        this.userList= new User[length];
+    // {null,null,null,...}
+    // null,null,null
+    // Obj, null,null,null
+
+    UserRepository(String name, int length) {
+        this.userList = new User[length];
         this.name = name;
     }
 
+    public boolean addUser(User user) {
+        if (index < userList.length) {
+            userList[index] = user;
+            index++;
+            return true;
+        } else
+            return false;
 
-    public boolean addUser(User user){
-        
+    }
+
+    public boolean removeUser(String username) {
+        boolean isFound = false;
+        User[] userListCopy = new User[userList.length];
+        int tempI = 0;
+        for (int i = 0; i <= index; i++) {
+            if (username.toLowerCase() != userList[i].getUsername().toLowerCase()) {
+                userListCopy[tempI] = userList[i];
+                tempI++;
+            } else {
+                isFound = true;
+            }
+
+        }
+        userList = userListCopy;
+        return isFound;
+
     }
 
     public String getName() {
         return name;
     }
-    public User[] getUserList(){
+
+    public User[] getUserList() {
         return userList;
     }
 }
